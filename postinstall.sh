58d2bf43abcbc71cbe44fc4dbba32d2e4d4bb31b
davidmarkclements <huperekchuno@googlemail.com>
2014-09-01 21:22:49 +0100

    postinst

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,14 @@
 mv seed-package.json package.json
 
+if [ -d ../../nscale-seed ]
+then echo "nscale-seed already exists!" && exit 1
+fi
+
 if [ -d ../../node_modules/nscale-seed ]
 then cd ../.. && mv node_modules/nscale-seed .
 fi
 
-if [ ! -d node_modules/* ]
+if [ ! "$(ls -A node_modules)" ]
 then rm -fr node_modules
 fi
 
